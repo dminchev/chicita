bd4ff76da0f36eed4847b6dd6810a9f16d048128
Martin Gogov <mgogov@gmail.com>
2012-05-20 14:15:01 +0300

    Adding categories controller and search action route

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,12 @@
+class CategoriesController < ApplicationController
+  def index
+  end
+
+  def show
+    render "look/show"
+  end
+
+  def search
+    # TODO search
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Chicita::Application.routes.draw do
   root :to => 'home#index'
 
   resources :look
+  resources :categories do
+    get :search
+  end
 end
